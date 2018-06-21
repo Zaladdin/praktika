ec091ff79d1f4d56b57fdc13d02ee1682fb2499e
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2018-06-21 22:50:20 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
+
 new Vue({
-  el:'#app',
-  data: {
-
-   bgc: {
-    backgroundColor:  ''
-  		}
-
-  	}
-
-
-
+  el: "#app",
+   data() {
+    return {
+      red: 255,
+      green: 255,
+      blue: 255
+    };
+  },
+  computed: {
+    appBackground() {
+      return `rgb(${this.red}, ${this.green}, ${this.blue})`;
+    }
+  }
 
 });
+
