c0e0f2e6c970f3774b08e5bd93216b61e72406b9
Furrya Black <furrya.black@gmail.com>
2018-06-21 22:23:19 +0300

    rbg instead of hex

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
+
 new Vue({
-  el:'#app',
-  data: {
-
-   bgc: {
-    backgroundColor:  ''
-  		}
-
-  	}
-
-
-
-
+  el: '#app',
+  data: function data() {
+    return {
+      style: { background: '' }
+    };
+  },
+  methods: {
+    hexToRgb: function hexToRgb(hex) {
+      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      const rgb = result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      } : null;
+      // this.style.background = hex;
+      this.style.background = 'rgb(' + [rgb && rgb.r, rgb && rgb.b, rgb && rgb.g].join(', ') + ')';
+    }
+  }
 });
